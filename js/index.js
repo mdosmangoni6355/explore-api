7d9e0a383354c56c6e1cfddec96f47cbde91fbbe
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-31 04:12:53 +0600

    index.js file added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function loadUsers1(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => showUsers1(users))
+}
+
+function showUsers1(users){
+    const ul = document.getElementById('users-list');
+    for(const user of users){
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+}
